62e8c382e5eb720522bd205b9ac044076504653c
Syfle <syfle.denisov@gmail.com>
2022-08-12 16:37:57 +0300

    1

--- src/App.js
+++ src/App.js
@@ -6,11 +6,11 @@ function App() {
   const [value, setValue] = useState('это первый чипс, это "второй," чипс')
   return (
     <div className="App">
-        <ChipsInput value={value} onChange={setValue} />
-        <p>
-          Строка: <br />
-          {value}
-        </p>
+      <ChipsInput value={value} onChange={setValue} />
+      <p>
+        Строка: <br />
+        {value}
+      </p>
     </div>
   )
 }
