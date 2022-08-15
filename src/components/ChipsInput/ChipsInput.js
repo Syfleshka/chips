7b8fef0ff145849d6dafbda79825d2e59e1e12e5
Syfle <syfle.denisov@gmail.com>
2022-08-15 14:48:27 +0300

    1

--- src/components/ChipsInput/ChipsInput.js
+++ src/components/ChipsInput/ChipsInput.js
@@ -5,17 +5,32 @@ import CloseIcon from './CloseIcon'
 
 function ChipsInput({ value, onChange }) {
   const [chips, setChips] = useState(makeChips(value))
-  const [error, setError] = useState({ text: '', key: -1 })
-  const errorMessages = {
-    quotes: 'Закройте кавычки с двух сторон'
+  const [error, setError] = useState({
+    text: 'Закройте кавычки с двух сторон',
+    keys: [],
+  })
+
+  const isQuotesClosed = (value) => (value.match(/"/g) || []).length % 2 === 0
+
+  const removeError = (key) => {
+    const errorKeys = error.keys.slice()
+    const index = error.keys.indexOf(key);
+    if (index !== -1) {
+      errorKeys.splice(index, 1);
+    }
+    setError({ ...error, keys: errorKeys })
+  }
+
+  const addError = (key) => {
+    setError({
+      ...error,
+      keys: [...error.keys, key],
+    })
   }
 
   const removeChip = (key = 0) => {
-    console.log('removing chip:', key)
     const chipsClone = chips.slice()
     chipsClone.splice(key, 1)
-    console.log('oldArr:', chips)
-    console.log('newArr:', chipsClone)
     setChips(chipsClone)
   }
 
@@ -25,51 +40,35 @@ function ChipsInput({ value, onChange }) {
     setChips(chipsClone)
   }
 
-  const handleMainChip = ({ event, key }) => {
+  const splitChips = ({ event, key }) => {
     const val = makeChips(event.target.value)
     helperSetChips({
       value: val,
       key,
     })
   }
+
   const mainChipConvertToChip = ({ event, key }) => {
     const chipValue = event.target.value
-    const isQuotesClosed = (chipValue.match(/"/g) || []).length % 2 === 0
     const isInputEmpty = !!event.target.value
-    if (isQuotesClosed) {
-      setError({...error, text: '', key: -1})
-      if (isInputEmpty) setChips([...chips, '' ])
+    if (isQuotesClosed(chipValue)) {
+      removeError(key)
+      if (isInputEmpty) setChips([...chips, ''])
     } else {
-      setError({...error, text: errorMessages.quotes,  key})
+      addError(key)
     }
   }
 
   const handleChip = ({ event, key }) => {
-    console.log(event.target.value)
     if (event.target.value === '') {
       removeChip(key)
-    }
-    helperSetChips({
-      value: [event.target.value],
-      key,
-    })
-  }
-
-  const convertToChip = ({ event, key }) => {
-    /*
-    const chipValue = event.target.value
-    const isQuotesClosed = (chipValue.match(/"/g) || []).length % 2 === 0
-    const isInputEmpty = !!event.target.value
-    if (isQuotesClosed) {
-      setError({...error, text: '', key: -1})
-      if (isInputEmpty) setChips([...chips, '' ])
     } else {
-      setError({...error, text: errorMessages.quotes,  key})
+      helperSetChips({
+        value: [event.target.value],
+        key,
+      })
     }
-    */
   }
-
-
 
   useEffect(() => {
     onChange(chips.join(','))
@@ -80,16 +79,21 @@ function ChipsInput({ value, onChange }) {
       <ul className="ChipsInput">
         {chips.map((chip, i) =>
           i < chips.length - 1 ? (
-            <li key={i} className="ChipsInput__tag">
+            <li
+              key={i}
+              className={`ChipsInput__tag${
+                error.keys.includes(i) ? ' error' : ''
+              }`}
+            >
               <span className="hidden">{chip}</span>
               <input
                 className="ChipsInput-tag__input"
                 type="text"
                 value={chip}
                 style={{
-                  width: chip.length + 'ch',
+                  width: chip.length + 1 + 'ch',
                 }}
-                onBlur={(e) => convertToChip({ event: e, key: chips.length - 1 })}
+                onBlur={(e) => splitChips({ event: e, key: i })}
                 onChange={(e) => handleChip({ event: e, key: i })}
               />
               <button className="close-button" onClick={() => removeChip(i)}>
@@ -99,15 +103,23 @@ function ChipsInput({ value, onChange }) {
           ) : null
         )}
         <input
-          className="ChipsInput__input"
+          className={`ChipsInput__input${
+            error.keys.includes(chips.length - 1) ? ' error' : ''
+          }`}
           type="text"
           value={chips[chips.length - 1]}
-          placeholder={chips.length > 1 ? "" : "Введите"}
-          onBlur={(e) => mainChipConvertToChip({ event: e, key: chips.length - 1 })}
-          onChange={(e) => handleMainChip({ event: e, key: chips.length - 1 })}
+          placeholder={chips.length > 1 ? '' : 'Введите'}
+          onBlur={(e) =>
+            mainChipConvertToChip({ event: e, key: chips.length - 1 })
+          }
+          onChange={(e) => splitChips({ event: e, key: chips.length - 1 })}
         />
       </ul>
-      <label className="ChipsInput__error">{error.text}</label>
+      <label
+        className={`ChipsInput__error${error.keys.length ? ' error' : ''}`}
+      >
+        {error.text}
+      </label>
     </div>
   )
 }
